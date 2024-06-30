8c01db16afe13001672b8bd48863dc3c7920a018
DiamondAngel1 <tararukolga78@gmail.com>
2024-07-01 00:52:38 +0300

    Function UserInput

--- Project3/FileName.cpp
+++ Project3/FileName.cpp
@@ -4,9 +4,25 @@
 #include <string.h>
 using namespace std;
 
+void UserInput(char*& user, int& length) {
+    cout << "¬вед≥ть р€док: ";
+    char temp[500];
+    cin.getline(temp, 500);
+    length = strlen(temp);
+    user = new char[length + 1];
+    for (int i = 0; i < length; ++i) {
+        user[i] = temp[i];
+    }
+    user[length] = '\0';
+}
+
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+
+    char* user = nullptr;
+    int userLength = 0;
+    UserInput(user, userLength);
 
     return 0;
 }
